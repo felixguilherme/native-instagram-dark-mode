6f9240d53466e776323ce91c430a6680acf13b28
Guilherme Félix <guilherme.f.santos@outlook.com.br>
2021-08-03 20:43:31 -0300

    Updated extension to work with multiparameter urls

--- background.js
+++ background.js
@@ -1,10 +1,17 @@
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        return {
-            redirectUrl: details.url + 
-                (details.url.indexOf("?theme=dark") == -1 ? "?theme=dark" : "")
-        };
-    },
-    {urls: ['*://*.instagram.com/*'], types: ['main_frame']},
-    ['blocking']
+  function (details) {
+    return {
+      redirectUrl:
+        details.url +
+        (details.url.indexOf("&theme=dark") == -1
+          ? details.url.indexOf("?theme=dark") == -1
+            ? details.url.indexOf("?") == -1
+              ? "?theme=dark"
+              : "&theme=dark"
+            : ""
+          : ""),
+    };
+  },
+  { urls: ["*://*.instagram.com/*"], types: ["main_frame"] },
+  ["blocking"]
 );
